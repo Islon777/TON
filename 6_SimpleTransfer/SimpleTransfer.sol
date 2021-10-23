7efc0502b82f4d889d207d9750f387e615f864dc
Islon777 <91132333+Islon777@users.noreply.github.com>
2021-10-23 12:19:41 +0700

    Update SimpleTransfer.sol

--- 6_SimpleTransfer/SimpleTransfer.sol
+++ 6_SimpleTransfer/SimpleTransfer.sol
@@ -12,8 +12,6 @@ pragma AbiHeader expire;
 
 contract SimpleTransfer {
 
-    enum paymentMethod { GetterFee, SenderFee, AllandDestroy } // Параметр для управления функцией sendTransaction
-
     constructor() public {
         require(tvm.pubkey() != 0, 101);
         require(msg.pubkey() == tvm.pubkey(), 102);
